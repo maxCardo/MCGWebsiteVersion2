a492f10dc566e893cade7d13ed96b15caea2f29b
Scott Carroll <41000349+ScottCarrollMBA@users.noreply.github.com>
2018-07-30 11:10:34 -0400

    Update send_post.php

--- send_post.php
+++ send_post.php
@@ -1,5 +1,14 @@
 <?php
-$connect = mysqli_connect("my host","my user","my passwrod","my db");
-mysqli_query($connect,"INSERT INTO posts (category, title, contents, tags)
-VALUES ('$_POST[post_category]', '$_POST[post_title]', '$_POST[post_contents]', '$_POST[post_tags]')";
+//Modify variable on form.html to match insert values
+//Account for address index being inserted into user table
+//Create form drop-down values that match table values, i.e. populate backend index of state with integer by querying state table first
+
+//Insert user into user table
+$connect = mysqli_connect("stoked-energy-210523:us-east1:mcgtechdb1","my user","my password","Site_Users");
+mysqli_query($connect,"INSERT INTO user_Site (siteuser_first, siteuser_Last, siteuser_added, siteuser_dob)
+VALUES ('$_POST[post_first]', '$_POST[post_last]', CURRENT_TIMESTAMP, '$_POST[post_dob]')";
+//Insert Address into Address Table
+$connect = mysqli_connect("stoked-energy-210523:us-east1:mcgtechdb1","my user","my password","Site_Users");
+mysqli_query($connect,"INSERT INTO user_Address (address_number, address_road, address_suff, address_added, address_city, address_state)
+VALUES ('$_POST[post_housenum]', '$_POST[post_street]', '$_POST[post_suffix]', CURRENT_TIMESTAMP, '$_POST[post_city]', '$_POST[post_state]')";
 ?>
